153abf117dc4a301330e07d802e7fd31d537fc12
Fabio <fabiop1995@gmail.com>
2026-06-01 10:34:12 +0200

    add new component navbar

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {
-
-}
+export class HomepageComponent {}
